04d6d58db537261d63f692ce34a6facc784f5aa3
Jerry <zhangji87@gmail.com>
2023-05-01 16:12:51 +0800

    Add sqlalchemy

--- quark/__init__.py
+++ quark/__init__.py
@@ -1,7 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+from . import default_settings
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+db = SQLAlchemy()
+
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    app.config.from_object(default_settings)
+    app.config.from_envvar('APP_CONFIG', silent=True)
+
+    db.init_app(app)
+
+    @app.route('/')
+    def hello():
+        from sqlalchemy import text
+        return str(db.session.execute(text('SELECT COUNT(*) FROM account')).scalar())
+
+    return app
