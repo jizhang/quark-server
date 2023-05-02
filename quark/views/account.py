8ef9ebb05544feb2f5565c2561e743fa2e42ac0c
Jerry <zhangji87@gmail.com>
2023-05-02 15:52:14 +0800

    Refactor save api

--- quark/views/account.py
+++ quark/views/account.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from datetime import datetime
 
 from flask import Blueprint, Response, jsonify, request
@@ -21,60 +22,74 @@ def account_save() -> Response:
     form = request.get_json()
     user_id = 1
 
-    if 'id' in form:
-        try:
-            account_id = int(form['id'])
-        except ValueError:
-            raise AppError('Invalid account ID')
+    if not isinstance(form, dict):
+        raise AppError('Invalid request body')
 
-        account = account_svc.get_account(user_id, account_id)
-        if account is None:
-            raise AppError('Account not found')
-
-        if not form.get('name'):
-            raise AppError('Account name cannot be empty.')
-
-        # Check name collision
-        by_name = account_svc.get_account_by_name(user_id, form['name'])
-        if by_name is not None and by_name.id != account.id:
-            raise AppError('Account name is duplicate.')
-
-        account.name = form['name']
-        db.session.commit()
-
-        return jsonify(id=account_id)
-
+    if form.get('id'):  # Editing
+        account = check_account_id(user_id, form['id'])
     else:
-        if not form.get('name'):
-            raise AppError('Account name cannot be empty.')
-
-        by_name = account_svc.get_account_by_name(user_id, form['name'])
-        if by_name is not None:
-            raise AppError('Account name is duplicate.')
-
-        if form.get('type') not in ['1', '2']:
-            raise AppError('Invalid account type')
-
-        if form.get('initial_balance'):
-            try:
-                initial_balance = float(form['initial_balance'])
-            except ValueError:
-                raise AppError('Invalid initial balance')
-        else:
-            initial_balance = 0
-
         account = Account()
+
+    if not form.get('name'):
+        raise AppError('Account name cannot be empty.')
+
+    # Check name collision
+    by_name = account_svc.get_account_by_name(user_id, form['name'])
+    if by_name is not None and (account.id is None or by_name.id != account.id):
+        raise AppError('Account name is duplicate.')
+
+    account.name = form['name']
+
+    if account.id is None:  # Adding
         account.user_id = user_id
-        account.name = form['name']
-        account.type = int(form['type'])
-        account.initial_balance = initial_balance
-        account.balance = initial_balance
+        account.type = check_account_type(form)
+        account.initial_balance = check_initial_balance(form)
+        account.balance = account.initial_balance
         account.order_num = account_svc.get_max_order_num(user_id) + 10
         account.created_at = datetime.now()
 
         db.session.add(account)
         db.session.flush()
-        account_id = account.id
-        db.session.commit()
 
-        return jsonify(id=account_id)
+    account_id = account.id
+    db.session.commit()
+    return jsonify(id=account_id)
+
+
+def check_account_id(user_id: int, account_id) -> Account:
+    try:
+        account_id = int(account_id)
+    except ValueError:
+        raise AppError('Invalid account ID')
+
+    account = account_svc.get_account(user_id, account_id)
+    if account is None:
+        raise AppError('Account not found')
+
+    return account
+
+
+def check_account_type(form: dict) -> int:
+    if not form.get('type'):
+        raise AppError('Account type cannot be empty.')
+
+    try:
+        account_type = int(form['type'])
+        if account_type not in (1, 2):
+            raise ValueError
+
+    except ValueError:
+        raise AppError('Invalid account type')
+
+    else:
+        return account_type
+
+
+def check_initial_balance(form: dict) -> Decimal:
+    if not form.get('initial_balance'):
+        return Decimal(0)
+
+    try:
+        return Decimal(form['initial_balance'])
+    except Exception:
+        raise AppError('Invalid initial balance')
