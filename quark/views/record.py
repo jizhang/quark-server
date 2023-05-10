07c4ffcae1f9c726294a13d9579ea7b7bce55c86
Jerry <zhangji87@gmail.com>
2023-05-10 16:27:51 +0800

    Serialize with schema

--- quark/views/record.py
+++ quark/views/record.py
@@ -1,5 +1,3 @@
-from typing import List
-from decimal import Decimal
 from datetime import datetime
 
 from flask import Blueprint, Response, jsonify, request
@@ -7,13 +5,11 @@ from flask_login import login_required, current_user
 from marshmallow import ValidationError
 
 from quark import db, AppError
-from quark.models.account import Account
 from quark.models.record import Record, RecordType
 from quark.services import account as account_svc
 from quark.services import category as category_svc
 from quark.services import record as record_svc
-from quark.utils import row_to_dict, rows_to_list
-from .record_item import record_item_schema
+from .record_item import RecordItemSchema
 from .record_form import record_form_schema
 
 bp = Blueprint('record', __name__, url_prefix='/api/record')
@@ -23,23 +19,14 @@ bp = Blueprint('record', __name__, url_prefix='/api/record')
 @login_required
 def record_list() -> Response:
     records = record_svc.get_list(current_user.id)
-    account_names = account_svc.get_name_mapping(current_user.id)
-    category_names = category_svc.get_name_mapping(current_user.id)
 
-    data: List[dict] = []
-    for row in records:
-        data.append({
-            'id': row.id,
-            'record_type': row.record_type,
-            'category_name': category_names.get(row.category_id),
-            'account_name': account_names.get(row.account_id),
-            'target_account_name': account_names.get(row.target_account_id),
-            'record_time': row.record_time,
-            'amount': row.amount,
-            'remark': row.remark,
-        })
+    schema = RecordItemSchema()
+    schema.context = {
+        'category_names': category_svc.get_name_mapping(current_user.id),
+        'account_names': account_svc.get_name_mapping(current_user.id),
+    }
 
-    return jsonify(data=record_item_schema.dump(data, many=True))
+    return jsonify(data=schema.dump(records, many=True))
 
 
 @bp.route('/get')
--- quark/views/record_item.py
+++ quark/views/record_item.py
@@ -4,12 +4,21 @@ from marshmallow import Schema, fields
 class RecordItemSchema(Schema):
     id = fields.Int()
     record_type = fields.Int()
-    category_name = fields.Str()
-    account_name = fields.Str()
-    target_account_name = fields.Str()
+    category_name = fields.Method('get_category_name')
+    account_name = fields.Method('get_account_name')
+    target_account_name = fields.Method('get_target_account_name')
     record_time = fields.DateTime()
     amount = fields.Decimal()
     remark = fields.Str()
+
+    def get_category_name(self, record):
+        return self.context['category_names'].get(record.category_id)
+
+    def get_account_name(self, record):
+        return self.context['account_names'].get(record.account_id)
+
+    def get_target_account_name(self, record):
+        return self.context['account_names'].get(record.target_account_id)
 
 
 record_item_schema = RecordItemSchema()
