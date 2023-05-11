959b3122ba7f28aed47a4d0cb94e35a083a709df
Jerry <zhangji87@gmail.com>
2023-05-11 17:53:18 +0800

    Organize files

--- quark/views/category/__init__.py
+++ quark/views/category/__init__.py
@@ -1,13 +1,5 @@
-from flask import Blueprint, Response, jsonify
-from flask_login import current_user
-
-from quark.services import category as category_svc
-from .category_schema import category_schema
+from flask import Blueprint
 
 bp = Blueprint('category', __name__, url_prefix='/api/category')
 
-
-@bp.route('/list')
-def category_list() -> Response:
-    rows = category_svc.get_list(current_user.id)
-    return jsonify(data=category_schema.dump(rows, many=True))
+from . import index
---
+++ quark/views/category/index.py
@@ -0,0 +1,13 @@
+from flask import Response, jsonify
+from flask_login import login_required, current_user
+
+from quark.services import category as category_svc
+from . import bp
+from .schemas.category import category_schema
+
+
+@bp.route('/list')
+@login_required
+def category_list() -> Response:
+    rows = category_svc.get_list(current_user.id)
+    return jsonify(data=category_schema.dump(rows, many=True))
